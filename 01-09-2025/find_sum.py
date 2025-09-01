9474808724945210bbcfbeba1419698701711d6e
Sivani Narala Setty (sinarala) <sinarala@cisco.com>
2025-09-01 11:01:09 +0530

    msg

---
+++ 01-09-2025/find_sum.py
@@ -0,0 +1,15 @@
+# find_sum
+'''
+findsum
+args: firstNumber, secondNumber
+returns sum of the numbers
+'''
+
+def find_sum(firstNum, secondNum):
+    result = firstNum + secondNum
+    return result
+
+print(find_sum(1,2))
+print(find_sum(24,8))
+
+
